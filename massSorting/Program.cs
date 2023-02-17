50b0bc57eeacae30c2c42bd68f41f484a9b56135
Ixor <dsv>
2023-02-17 10:05:49 +0300

    Добавьте файлы проекта.

---
+++ massSorting/Program.cs
@@ -0,0 +1,34 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine();
+        String[] strings = Console.ReadLine().Split(" ");
+        int[] nums = new int[strings.Length];
+        for (int i = 0;i<strings.Length;i++)
+        {
+            nums[i] = Convert.ToInt32(strings[i]);
+        }
+        sort(nums);
+    }
+    private static void sort(int[] nums)
+    {
+        int temp = 0;
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if ((i+1!=nums.Length) && nums[i] > nums[i + 1])
+            {
+                temp = nums[i];
+                nums[i] = nums[i + 1];
+                nums[i + 1] = temp;
+                i = -1;
+                foreach(int number in nums)
+                {
+                    Console.Write($"{number} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
